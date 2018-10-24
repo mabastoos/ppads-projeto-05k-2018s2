e5f0d577faa484e62d22b11ddb39f93fbc303b60
Marcella Bastos <mah.baastos@gmail.com>
2018-10-24 20:35:56 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import background from './imagens/back.jpg';
 import './App.css';
-import ResponsiveMenu from 'react-responsive-navbar';
+/* import ResponsiveMenu from 'react-responsive-navbar'; */
 import {Link} from 'react-router-dom';
 
 class App extends Component {
